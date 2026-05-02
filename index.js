63a85165915f180b86b781dbb01b8ce53b7e85a0
Leslie Cherop <lesliecherop9@gmail.com>
2026-05-03 02:12:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here!
+function displayPosts(posts) {
+    const ul=document.getElementById("post-list");
+    ul.innerHTML ="";
+    posts.forEach(post => {
+        const li = document.createElement("li");
+        const hi=document.createElement("h1");
+        hi.testContent=post.title;
+        const p=document.createElement("p");
+        p.textContent=post.body;
+        li.appendChild(h1);
+        li.appendChild(p);
+        ul.appendChild(li);
+    });
+}
+async function getPosts(){
+    try{
+        const response=await fetch("https://jsonplaceholder.typicode.com/posts");
+        const data=await response.json();
+        displayPosts(data);
+    }catch(error){
+        console.log("Erro fetching posts:",error);
+    }
+    }
+}
